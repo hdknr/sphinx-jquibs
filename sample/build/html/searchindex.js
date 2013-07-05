c6804ea925860d740e305810bccfb5d7ab647d12
hdknr <gmail@hdknr.com>
2013-07-06 00:51:32 +0900

    Ignore build

--- sample/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,sphinx:0,welcom:0,modul:0,bootstrap:0,theme:0,indic:0,content:0,jqueri:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Sphinx Theme with jQuery UI Bootstrap&#8217;s documentation!"],objnames:{},filenames:["index"]})
